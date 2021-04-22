f7415e848949830b8c83a5c6df1e7105d660f023
Michael Grossbach <michael.grossbach@hmtm-hannover.de>
2021-04-22 17:54:10 +0200

    Adds data set

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Practicing Times of Musicians.
+#'
+#' Students at a conservatory were asked to retrospectively estimate the average
+#' daily time spent at the (main) instrument in seven age brackets.
+#'
+#' @format A data frame with 68 rows and 10 variables:
+#' \describe{
+#'   \item{ID}{subject ID}
+#'   \item{DOB}{Date of birth, in YYYY-MM-DD format}
+#'   \item{Date}Date of lab visit}
+#'   \item{0-5}{Mean daily practicing time in the age bracket 0 - 5}
+#'   \item{6-8}{Mean daily practicing time in the age bracket 6 - 8}
+#'   \item{9-12}{Mean daily practicing time in the age bracket 9 - 12}
+#'   \item{13-16}{Mean daily practicing time in the age bracket 13 - 16}
+#'   \item{17-20}{Mean daily practicing time in the age bracket 17 - 20}
+#'   \item{21-25}{Mean daily practicing time in the age bracket 21 - 25}
+#'   \item{26-30}{Mean daily practicing time in the age bracket 26 - 30}
+"practicetimes"
