be53094b51be3358c20f6f0e2ab02f243972fa0f
Michael Grossbach <michael.grossbach@hmtm-hannover.de>
2021-04-28 06:01:19 +0200

    Add bracket_multipliers(), col_lastentry(); work on vignette

--- R/praktikr.R
+++ R/praktikr.R
@@ -63,3 +63,85 @@ days_since_bday <- function(dob, today = Sys.Date()) {
   days[pos_idx] <- date_diff[pos_idx]
   return(days)
 }
+#' Age Bracket Multiplier.
+#'
+#' @param df A data.frame (also) containing the subjective and retrospectively
+#'   provided practicing times of each participant.
+#' @param bracket_idx A numerical vector indexing the bracket columns. If not
+#'   provided, the entire data.frame is assumed to only contain age bracket
+#'   columns.
+#' @param sep A character string used to separate the beginning and ending ages
+#'   in age bracket column names.
+#'
+#' @return A numerical vector the same length as \code{nrow(df)}, providing the
+#'   indices of the right-most non-NA entry in each row of \code{df}.
+#'
+#' @export
+#'
+#' @examples
+#' d_f <- data.frame(`10-12` = c(1.5, 1.0, 0.75), `13-14` = c(3, 0.75, 1.0), check.names = FALSE)
+#' bracket_multipliers(d_f)
+bracket_multipliers <- function(df, bracket_idx = 1:ncol(df), sep = "-") {
+  nrow_df <- nrow(df)
+  ddf <- as.data.frame(matrix(NA,
+                              nrow = nrow_df,
+                              ncol = length(bracket_idx)))
+  names_ddf <- names(df)[bracket_idx]
+  durations <- names_to_durations(names_ddf,
+                               sep = sep)
+  ddf <- as.data.frame(matrix(durations,
+                              nrow = nrow_df,
+                              ncol = ncol(ddf),
+                              byrow = TRUE))
+  names(ddf) <- paste("multiplier",
+                      names_ddf,
+                      sep = "_")
+  return(ddf)
+}
+#' Last Column With (Non-NA) Content.
+#'
+#' Returns the column index of the right-most non-NA entry of each row in a
+#' data.frame.
+#'
+#' @param df A data.frame
+#'
+#' @return A vector with column names.
+#' @export
+#'
+#' @examples
+#' d_f <- data.frame(a = c("a", "b", NA), b = c(1, NA, 3))
+#' col_lastentry(d_f)
+col_lastentry = function(x) {
+  idx = ifelse(is.na(x),
+               0L,
+               col(x))
+  apply(idx, 1, max)
+}
+add_daily_hours <- function(x, bracket_idx = ncol(x), bracket_mult = NULL, append = TRUE) {
+  cbind(x,
+        x[ , bracket_idx] * bracket_mult * 365.25)
+}
+#' Convert Column Names to Age Bracket Durations.
+#'
+#' @param col_names A character vector of age brackets.
+#' @param sep A character used to separate the lower and upper bound of the
+#'   bracket ranges.
+#'
+#' @return A numerical vector with age bracket durations (usually in years).
+#' @export
+#'
+#' @examples
+#' d_f <- data.frame(`10-12` = c(1.5, 1.0, 0.75), `13-14` = c(3, 0.75, 1.0), check.names = FALSE)
+#' names_to_durations(names(d_f), sep = "-")
+names_to_durations <- function(col_names = NULL, sep = NULL) {
+  n_names <- length(col_names)
+  duration <- vector(mode = "integer", length = n_names)
+  for (n in 1:length(col_names)) {
+    split_name <- strsplit(col_names[n],
+                           split = sep)
+    lb <- strtoi(split_name[[1]][1])
+    ub <- strtoi(split_name[[1]][2])
+    duration[n] <- length(lb:ub)
+  }
+  return(duration)
+}
